d33c07dc91a7ff1ee64ea45b9d666cf740461e44
prasannadigital <prasannav@digitalrupay.com>
2018-08-04 15:42:20 +0530

    logout button

--- src/app/time-clocks/time-clocks.component.ts
+++ src/app/time-clocks/time-clocks.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { TimeClokServiceService} from '../services/time-clok-service.service';
+import { TimeClokServiceService} from '../services/time-clok-service.service'
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AlertComponent } from 'ngx-bootstrap/alert/alert.component';
+import { Globals } from '../global/global-urls';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Component({
   selector: 'time-clocks',
   templateUrl: './time-clocks.component.html',
@@ -7,9 +12,12 @@ import { TimeClokServiceService} from '../services/time-clok-service.service';
 })
 export class TimeClocksComponent implements OnInit {
 
-  constructor(private service:TimeClokServiceService) { }
+  constructor(private service:TimeClokServiceService,private http: HttpClient, private router: Router, private globals: Globals) { }
 
   today: number;
+  password = "";
+  mailId = "";
+  alerts: any[] = [];
 
   ngOnInit() {
     this.getTimeAndDate();
@@ -19,5 +27,27 @@ export class TimeClocksComponent implements OnInit {
   }
  getTimeAndDate(){
   this.today = Date.now();
+ }
+ 
+ loginSubmite()
+ {
+  var data = {
+    password: this.password,
+    email_id: this.mailId
+  }
+  if (this.mailId && this.password) {
+    this.http.post(this.globals.api + 'auth/login', data).subscribe(loginData => {
+      if (Object.keys(loginData).length) {
+        sessionStorage.setItem('userSession', JSON.stringify(loginData));
+        this.router.navigate(['time-clock']);
+      }
+    });
+  } else {
+    this.alerts = [{
+      type: 'danger',
+      msg: `Invalid credentials`,
+      timeout: 1000
+    }];
+  }
  }
 }
