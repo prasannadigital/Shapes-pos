3c3df6439524151ebda51eaccff0775cda93ef1c
prasannadigital <prasannav@digitalrupay.com>
2018-08-06 11:17:30 +0530

    signin

--- src/app/time-clocks/time-clocks.component.ts
+++ src/app/time-clocks/time-clocks.component.ts
@@ -4,6 +4,7 @@ import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from
 import { AlertComponent } from 'ngx-bootstrap/alert/alert.component';
 import { Globals } from '../global/global-urls';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+declare var $: any;
 
 @Component({
   selector: 'time-clocks',
@@ -15,6 +16,13 @@ export class TimeClocksComponent implements OnInit {
   constructor(private service:TimeClokServiceService,private http: HttpClient, private router: Router, private globals: Globals) { }
 
   today: number;
+  // password = "";
+  // mailId = "";
+  
+  // alerts: any[] = [];
+  // loginData=new Array();
+  
+
   
 
   ngOnInit() {
@@ -22,10 +30,34 @@ export class TimeClocksComponent implements OnInit {
             setInterval(() => {
                 this.getTimeAndDate(); 
                 }, 1000);
+              
   }
  getTimeAndDate(){
   this.today = Date.now();
  }
- 
+//  loginSubmite(){
+//   var data = {
+//     password: this.password,
+//     email_id: this.mailId
+//   }
+//   if (this.mailId && this.password) {
+//     this.http.post(this.globals.api + 'auth/login', data).subscribe(loginData => {
+//       console.log(loginData)
+//       if (loginData.status==200) {
+//         console.log('hii')
+//         $("#allInfo").modal('hide');
+//         this.router.navigate(['time-clocks']);
+//       }
+//     });
+//   } else {
+//     this.alerts = [{
+//       type: 'danger',
+//       msg: `Invalid credentials`,
+//       timeout: 1000
+//     }];
+//   }
+
+  
+// }
  
 }
