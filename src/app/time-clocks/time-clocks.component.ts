d815535b48ceddf26fe620ecca9849677215c000
prasannadigital <prasannav@digitalrupay.com>
2018-08-07 18:21:09 +0530

    time-clocks

--- src/app/time-clocks/time-clocks.component.ts
+++ src/app/time-clocks/time-clocks.component.ts
@@ -38,7 +38,7 @@ export class TimeClocksComponent implements OnInit {
   test:'text';
   test1:any;
   data: any = {
-    'id':'',
+    'time_clock_id':'',
     'emp_id': '',
     'check_in_time': null,
     'break_in1': null,
@@ -82,8 +82,11 @@ export class TimeClocksComponent implements OnInit {
     this.test1=response;
     console.log(this.test1);
     console.log(this.test1.emp_id);
+    //this.data.id=this.test1.id;
+    this.data.time_clock_id=this.test1.time_clock_id;
     this.data.emp_id=this.test1.emp_id;
     this.data.check_in_time=this.test1.check_in_time;
+    this.data.check_out_time=this.test1.check_out_time;
     this.data.break_in1=this.test1.break_in1;
     this.data.break_out1=this.test1.break_out1;
     this.data.break_in2=this.test1.break_in2;
@@ -107,28 +110,7 @@ export class TimeClocksComponent implements OnInit {
     }];
   }
 }
-saveClockTimings(){
- var data={
-  emp_id:this.emp_id,
-  check_in_time:this.check_in_time,
-  check_out_time:this.check_out_time,
-  break_in1:this.break_in1,
-  break_out1:this.break_out1,
-  break_in2:this.break_in2,
-  break_out2:this.break_out2,
-  break_in3:this.break_in3,
-  break_out3:this.break_out3,
-  break_in4:this.break_in4,
-  break_out4:this.break_out4,
-  check_in_date:this.check_in_date,
-  modified_by:this.modified_by,
-  remarks:this.remarks,
-  total_hours:this.total_hours
- } 
- this.service.saveInandOutTime(data);
-}
 clockInTime(){
-
   this.data.check_in_time = Date.now();
  console.log(this.data.check_in_time);
  this.service.saveInandOutTime(this.data).subscribe(response => {
@@ -138,32 +120,43 @@ clockInTime(){
 }
 clockOutTime(){
   this.data.check_out_time = Date.now();
-  this.saveClockTimings();
+  this.service.saveInandOutTime(this.data).subscribe(response => {
+    console.log(response);
+  });
   //alert("hfjhdsjfh");
 }
 breakOutTime(){
   console.log(this.break_out1);
 if(this.data.break_out1=== null){
   this.data.break_out1 = Date.now();
-  this.saveClockTimings();
+  console.log(this.data);
+  this.service.saveInandOutTime(this.data).subscribe(response => {
+    console.log(response);
+  });
   return true;
   //alert("hfjhdsjfh");
 }
 if(this.data.break_out2=== null){
   this.data.break_out2 = Date.now();
-  this.saveClockTimings();
+  this.service.saveInandOutTime(this.data).subscribe(response => {
+    console.log(response);
+  });
   return true;
   //alert("hfjhdsjfh");
 }
 if(this.data.break_out3=== null){
   this.data.break_out3 = Date.now();
-  this.saveClockTimings();
+  this.service.saveInandOutTime(this.data).subscribe(response => {
+    console.log(response);
+  });
   return true;
   //alert("hfjhdsjfh");
 }
 if(this.data.break_out4=== null){
   this.data.break_out4 = Date.now();
-  this.saveClockTimings();
+  this.service.saveInandOutTime(this.data).subscribe(response => {
+    console.log(response);
+  });
   return true;
   //alert("hfjhdsjfh");
 }
@@ -172,25 +165,33 @@ breakInTime(){
   console.log(this.break_out1);
 if(this.data.break_in1=== null){
   this.data.break_in1 = Date.now();
-  this.saveClockTimings();
+  this.service.saveInandOutTime(this.data).subscribe(response => {
+    console.log(response);
+  });
   return true;
   //alert("hfjhdsjfh");
 }
 if(this.data.break_in2=== null){
   this.data.break_in2 = Date.now();
-  this.saveClockTimings();
+  this.service.saveInandOutTime(this.data).subscribe(response => {
+    console.log(response);
+  });
   return true;
   //alert("hfjhdsjfh");
 }
 if(this.data.break_in3=== null){
   this.data.break_in3 = Date.now();
-  this.saveClockTimings();
+  this.service.saveInandOutTime(this.data).subscribe(response => {
+    console.log(response);
+  });
   return true;
   //alert("hfjhdsjfh");
 }
 if(this.data.break_in4=== null){
   this.data.break_in4 = Date.now();
-  this.saveClockTimings();
+  this.service.saveInandOutTime(this.data).subscribe(response => {
+    console.log(response);
+  });
   return true;
   //alert("hfjhdsjfh");
 }
