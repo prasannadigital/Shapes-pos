0c7e247c5c860d0eeceff55d7aaa65eb3f8f228a
rajanipos <rajani@digitalrupay.com>
2018-09-19 17:48:56 +0530

    add packages and sales button color

--- src/app/manager-tool/add-packages/add-packages.component.ts
+++ src/app/manager-tool/add-packages/add-packages.component.ts
@@ -1,36 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import {MultiSelectModule} from 'primeng/multiselect';
-import {SelectItem} from 'primeng/api';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SelectItem } from 'primeng/api';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { PackagesServiceService } from '../../services/packages-service.service';
 @Component({
   selector: 'app-add-packages',
   templateUrl: './add-packages.component.html',
   styleUrls: ['./add-packages.component.css']
 })
 export class AddPackagesComponent implements OnInit {
-  packs:SelectItem[]=[];
+  packs: SelectItem[] = [];
   selectedPack: string[] = [];
-  serviceData:any;
-  constructor(private router: Router,private http: HttpClient) {
-     
-    }
-    ngOnInit() {
-      this.http.get(environment.host + 'services').subscribe(data => {
-        this.serviceData = data;
-     
-        console.log(this.serviceData.length);
-        for(let i=0;i<this.serviceData.length;i++){
-          this.packs.push( {label: this.serviceData[i].service_name, value: this.serviceData[i].service_name })
-        }
-     });
-    }
+  serviceData: any;
+  packageName = '';
+  catId = '';
+  subCatId = '';
+  marked = false;
+  serviceId = '';
 
+  constructor(private router: Router, private http: HttpClient, private service: PackagesServiceService) {
 
+  }
+
+  ngOnInit() {
+    this.http.get(environment.host + 'services').subscribe(data => {
+      this.serviceData = data;
+      console.log(this.serviceData.length);
+      for (let i = 0; i < this.serviceData.length; i++) {
+        this.packs.push({ label: this.serviceData[i].service_name, value: { service_id: this.serviceData[i].service_id, service_price: this.serviceData[i].service_price, service_name: this.serviceData[i].service_name } })
+      }
+    });
+  }
+
+  deleteService(index) {
+    this.selectedPack.splice(index, 1);
+    this.getServiceId(this.selectedPack);
+  }
+
+  getServiceId(val) {
+    this.serviceId = '';
+    for (let i = 0; i < val.length; i++) {
+      this.serviceId = this.serviceId + val[i].service_id + " , ";
+      console.log(this.serviceId);
+    }
+    this.serviceId = this.serviceId.substring(0, this.serviceId.length - 1);
+  }
+
+  addPackage() {
+    var data = {
+      sub_cat_id: 15,
+      cat_id: 12,
+      package_name: this.packageName,
+      package_services: this.serviceId
+    }
+    console.log(this.packageName);
+    console.log(this.serviceId);
+    this.service.savePackage(data).subscribe(response => {
+      console.log(response.json);
+    })
+  }
 
   backToMembership() {
     this.router.navigate(['management']);
+  }
+  
+  onChange() {
+    console.log(this.selectedPack);
+    this.getServiceId(this.selectedPack);
   }
 
 }
--- src/app/services/packages-service.service.ts
+++ src/app/services/packages-service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../environments/environment';
+import { Http } from '@angular/http';
 @Injectable({
   providedIn: 'root'
 })
 export class PackagesServiceService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  savePackage(pack:any){
+    return this.http.post(environment.host + 'packages', pack);
+  }
+  getPackage(){
+    return this.http.get(environment.host + 'packages');
+  }
 }
