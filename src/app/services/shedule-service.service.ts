d952adda9793b77f110c77e7c964a7b62fec0423
prasanna1126 <venkatperumalla1992@gmail.com>
2018-09-27 11:01:07 +0530

    Appopiontments API

--- src/app/services/shedule-service.service.ts
+++ src/app/services/shedule-service.service.ts
@@ -17,6 +17,12 @@ export class SheduleServiceService {
   getAllLocations() {
     return this.http.get(environment.host + 'branches');
   }
+  getStaffByLocation(id: number) {
+    return this.http.get(environment.host + 'branch-emp/'+ id );
+  }
+  getStaffAppointments(id1: number,id2:number) {
+    return this.http.get(environment.host + 'emp-branch-appt/'+ id1+'/'+id2);
+  }
   getEmpAppointments(startdate: any, enddate: any, emp_id: number, branchid: number) {
     return this.http.get(environment.host + 'orders/appointment?startdate=' + startdate + '&enddate=' + enddate + '&emp_id=' + emp_id + '&branchid=' + branchid);
     // http://ec2-54-88-194-105.compute-1.amazonaws.com:3001/orders/appointment?startdate=2018-01-01&enddate=2018-11-11&emp_id=1&branchid=1
