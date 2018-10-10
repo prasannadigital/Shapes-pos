65a50ffb688a7fb112ee1d43bfbe8aa0703a96d0
rajanipos <rajani@digitalrupay.com>
2018-10-10 11:13:45 +0530

    payment-processing design

--- src/app/reports/Payment-Processing/autopay-summary/autopay-summary.component.ts
+++ src/app/reports/Payment-Processing/autopay-summary/autopay-summary.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-autopay-summary',
   templateUrl: './autopay-summary.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutopaySummaryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
-
+  backToReport() {
+    this.router.navigate(['reports'])
+  }
 }
