e1f81e275bb7ae0798815961e4bf405d921097a6
prasanna1126 <venkatperumalla1992@gmail.com>
2018-10-24 14:13:00 +0530

    header and footer design issue

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -24,47 +24,79 @@ export class HeaderComponent implements OnInit {
   constructor(private http: HttpClient, private router: Router, private globals: Globals) {
     
     if (this.router.url == '/appointments') {
+      this.removeActiveClass();
       $(document).ready(function () {
         $("#__appt").addClass("active");
       });
     }
 
     if (this.router.url == '/dashboard') {
+      this.removeActiveClass();
       $(document).ready(function () {
         $("#__sale").addClass("active");
       });
     }
-
+    if (this.router.url == '/time-clocks') {
+      this.removeActiveClass();
+      $(document).ready(function () {
+        $("#__timeclock").addClass("active");
+      });
+    }
     if (this.router.url == '/schedule') {
+      this.removeActiveClass();
       $(document).ready(function () {
         $("#__schedule").addClass("active");
       });
     }
 
     if (this.router.url == '/reports') {
+      this.removeActiveClass();
       $(document).ready(function () {
         $("#__reports").addClass("active");
       });
     }
 
     if (this.router.url == '/management') {
+      this.removeActiveClass();
       $(document).ready(function () {
         $("#__manager").addClass("active");
       });
     }
 
     if (this.router.url == '/inventory') {
+      this.removeActiveClass();
       $(document).ready(function () {
         $("#__inventory").addClass("active");
       });
     }
+    if (this.router.url == '/setup') {
+      this.removeActiveClass();
+      $(document).ready(function () {
+        $("#__setup").addClass("active");
+      });
+    }
   }
+
 
   ngOnInit() {
 
   }
 
+  removeActiveClass(){
+    $(document).ready(function () {
+      $("#__sale").removeClass("active");
+      $("#__appt").removeClass("active");
+      $("#__timeclock").removeClass("active");
+      $("#__schedule").removeClass("active");
+      $("#__reports").removeClass("active");
+      $("#__manager").removeClass("active");
+      $("#__inventory").removeClass("active");     
+      $("#__setup").removeClass("active");  
+    });
+  }
+
   redirectToDashbaord() {
+    this.removeActiveClass();
     this.router.navigate(['dashboard']);
     $("#__sale").click(function () {
       $("#__sale").addClass("active");
@@ -72,6 +104,7 @@ export class HeaderComponent implements OnInit {
   }
 
   redirectToAppointment() {
+    this.removeActiveClass();
     this.router.navigate(['appointments'])
     $("#__appt").click(function () {
       $("#__appt").addClass("active");
@@ -79,10 +112,15 @@ export class HeaderComponent implements OnInit {
   }
 
   redirectToTimeClocks() {
+    this.removeActiveClass();
     this.router.navigate(['time-clocks'])
+    $("#__timeclock").click(function () {
+      $("#__timeclock").addClass("active");
+    });
   }
 
   redirectToSchedule() {
+    this.removeActiveClass();
     this.router.navigate(['schedule'])
     $("#__schedule").click(function () {
       $("#__schedule").addClass("active");
@@ -90,6 +128,7 @@ export class HeaderComponent implements OnInit {
   }
 
   redirectToReport() {
+    this.removeActiveClass();
     this.router.navigate(['reports'])
     $("#__reports").click(function () {
       $("#__reports").addClass("active");
@@ -97,6 +136,7 @@ export class HeaderComponent implements OnInit {
   }
 
   redirectToManager() {
+    this.removeActiveClass();
     this.router.navigate(['management'])
     $("#__manager").click(function () {
       $("#__manager").addClass("active");
@@ -104,6 +144,7 @@ export class HeaderComponent implements OnInit {
   }
 
   redirectToInventory() {
+    this.removeActiveClass();
     this.router.navigate(['inventory'])
     $("#__inventory").click(function () {
       $("#__inventory").addClass("active");
@@ -111,6 +152,7 @@ export class HeaderComponent implements OnInit {
   }
 
   redirectToSetup() {
+    this.removeActiveClass();
     this.router.navigate(['setup'])
     $("#__setup").click(function () {
       $("#__setup").addClass("active");
