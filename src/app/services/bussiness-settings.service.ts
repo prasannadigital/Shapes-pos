79071e4c696cd5fc32890b919c205155b429944b
prasanna1126 <venkatperumalla1992@gmail.com>
2018-10-27 14:28:43 +0530

    locations and app listings done

--- src/app/services/bussiness-settings.service.ts
+++ src/app/services/bussiness-settings.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BussinessSettingsService {
+  
+  constructor(private http: Http) { }
+  public editBussinessAddress(data: any) {
+    return this.http.post(environment.host + 'locations', data)
+  }
 
-  constructor() { }
+  public getBussinessAddress() {
+    return this.http.get(environment.host + 'locations');
+  }
 }
--- src/app/setup/business-settings/business-settings.component.ts
+++ src/app/setup/business-settings/business-settings.component.ts
@@ -13,6 +13,28 @@ import { Http } from '@angular/http';
   styleUrls: ['./business-settings.component.css']
 })
 export class BusinessSettingsComponent implements OnInit {
+  locations: any = {
+    'location_id': '',
+    'loc_bussiness_address': '',
+    'loc_address2': '',
+    'loc_city': '',
+    'loc_stateprovince': '',
+    'loc_postal_code': '',
+    'loc_phone': '',
+    'loc_ext': '',
+    'loc_contact_name': '',
+    'loc_shipping_address': '',
+    'loc_region': '',
+    'loc_timezone': '',
+    'loc_meet_your_client': '',
+    'loc_business_desc': '',
+    'loc_loc_desc': '',
+    'loc_online_buss_address': '',
+    'loc_online_phone': '',
+    'loc_online_ext': '',
+    'loc_online_contact_name': '',
+    'loc_online_shipping_address':''
+  };
   packs: SelectItem[] = [];
   selectedPack: string[] = [];
   serviceData: any;
@@ -24,6 +46,7 @@ export class BusinessSettingsComponent implements OnInit {
   public date1: any;
   public date2: any;
   serviceId = '';
+  bussinessAddressData: any;
   constructor(private http: Http,private service:BussinessSettingsService,private router:Router,private serviceData1: SheduleServiceService,) { }
 
   ngOnInit() {
@@ -35,6 +58,31 @@ export class BusinessSettingsComponent implements OnInit {
         this.packs.push({ label: this.serviceData[i].category_name, value: { cat_id: this.serviceData[i].cat_id,category_name: this.serviceData[i].category_name} })
       }
     });
+   this.getBussinessAddressForShapes();
+  }
+  getBussinessAddressForShapes(){
+    this.service.getBussinessAddress().subscribe(res => {
+      this.bussinessAddressData = res.json().result;
+      console.log( this.bussinessAddressData)
+      this.locations.location_id=this.bussinessAddressData[0].location_id;
+      this.locations.loc_bussiness_address=this.bussinessAddressData[0].loc_bussiness_address;
+      this.locations.loc_address2=this.bussinessAddressData[0].loc_address2;
+      this.locations.loc_city=this.bussinessAddressData[0].loc_city;
+      this.locations.loc_stateprovince=this.bussinessAddressData[0].loc_stateprovince;
+      this.locations.loc_postal_code=this.bussinessAddressData[0].loc_postal_code;
+      this.locations.loc_phone=this.bussinessAddressData[0].loc_phone;
+      this.locations.loc_contact_name=this.bussinessAddressData[0].loc_contact_name;
+      this.locations.loc_shipping_address=this.bussinessAddressData[0].loc_shipping_address;
+      this.locations.loc_region=this.bussinessAddressData[0].loc_region;
+      this.locations.loc_timezone=this.bussinessAddressData[0].loc_timezone;
+      this.locations.loc_meet_your_client=this.bussinessAddressData[0].loc_meet_your_client;
+      this.locations.loc_business_desc=this.bussinessAddressData[0].loc_business_desc;
+      this.locations.loc_loc_desc=this.bussinessAddressData[0].loc_loc_desc;
+      this.locations.loc_online_buss_address=this.bussinessAddressData[0].loc_online_buss_address;
+      this.locations.loc_online_phone=this.bussinessAddressData[0].loc_online_phone;
+      this.locations.loc_online_contact_name=this.bussinessAddressData[0].loc_online_contact_name;
+      this.locations.loc_online_shipping_address=this.bussinessAddressData[0].loc_online_shipping_address;
+    })
   }
   backToSetup(){
     this.router.navigate(['setup']);
@@ -77,6 +125,30 @@ export class BusinessSettingsComponent implements OnInit {
   onChange() {
     console.log(this.selectedPack);
     this.getServiceId(this.selectedPack);
+  }
+  saveBussinessAddress(){
+    var data = {
+      location_id: this.locations.location_id,
+      loc_bussiness_address: this.locations.loc_bussiness_address,
+      loc_address2: this.locations.loc_address2,
+      loc_city:this.locations.loc_city,
+      loc_stateprovince: this.locations.loc_stateprovince,
+      loc_postal_code: this.locations.loc_postal_code,
+      loc_phone: this.locations.loc_phone,
+      loc_contact_name:this.locations.loc_contact_name,
+      loc_shipping_address: this.locations.loc_shipping_address,
+      loc_region: this.locations.loc_region,
+      loc_timezone: this.locations.loc_timezone,
+      loc_meet_your_client:this.locations.loc_meet_your_client,
+      loc_business_desc: this.locations.loc_business_desc,
+      loc_online_buss_address: this.locations.loc_online_buss_address,
+      loc_online_phone: this.locations.loc_online_phone,
+      loc_online_contact_name:this.locations.loc_online_contact_name,
+      loc_online_shipping_address:this.locations.loc_online_shipping_address,
+      loc_loc_desc:this.locations.locations.loc_loc_desc
+    }
+    this.service.editBussinessAddress(data).subscribe(data => {
+    });
   }
 }
 
