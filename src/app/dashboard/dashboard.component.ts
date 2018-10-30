3dfd2cc938b9e67e537ad647dfb56a73acccd5ff
prasanna1126 <venkatperumalla1992@gmail.com>
2018-10-30 17:26:53 +0530

    haeder tab active issue resolved

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { getLocaleTimeFormat } from '@angular/common';
 import { NgxSpinnerService } from 'ngx-spinner';
+declare var $: any;
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -22,6 +23,24 @@ userData:any;
      sessionStorage.removeItem('backBtnTimeclocks');     
      sessionStorage.removeItem('backBtnShedule');//remove Back btn popup
 this.getUser();
+if (this.router.url == '/dashboard') {
+  this.removeActiveClass();
+  $(document).ready(function () {
+    $("#__sale").addClass("active");
+  });
+}
+  }
+  removeActiveClass(){
+    $(document).ready(function () {
+      $("#__sale").removeClass("active");
+      $("#__appt").removeClass("active");
+      $("#__timeclock").removeClass("active");
+      $("#__schedule").removeClass("active");
+      $("#__reports").removeClass("active");
+      $("#__manager").removeClass("active");
+      $("#__inventory").removeClass("active");     
+      $("#__setup").removeClass("active");  
+    });
   }
   getUser(){
   if(sessionStorage){
